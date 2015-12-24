8965b7e41741143d72a6a63f08ecbd37c3bff3bb
zhengjunwei <cxrhphp@qq.com>
2015-12-25 00:59:32 +0800

    gulp success

---
+++ examples/1.js
@@ -0,0 +1,9 @@
+/* 
+* @Author: eric zheng
+* @Date:2015-12-25 00:57:33
+* @Last Modified by:eric zheng
+* @Last Modified time:2015-12-25 00:57:45
+* @WebSite:moyu-edu.com
+* @Motto:stay foolish stay hungry
+*/
+alert(1);
