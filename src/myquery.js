d4dd3aeccb0f1ffe0e18c1e33c19484d2646151c
zhengjunwei <cxrhphp@qq.com>
2015-12-25 16:58:51 +0800

    map

--- src/myquery.js
+++ src/myquery.js
@@ -2,11 +2,11 @@
 * @Author: eric zheng
 * @Date:2015-12-25 10:22:45
 * @Last Modified by:eric zheng
-* @Last Modified time:2015-12-25 15:43:17
+* @Last Modified time:2015-12-25 16:58:03
 * @WebSite:moyu-edu.com
 * @Motto:stay foolish stay hungry
 */
-!(function(global,factory){
+!( function( global,factory ){
 	var myQuery = factory(global);
 	if(typeof module === "object" && typeof module.exports === "object"){
 		module.exports = global.document ?
@@ -26,9 +26,90 @@
 
 	};
 
-	myQuery.fn = function(){
-		alert(1);
+
+
+/*==============================函数式API start ==================================================*/
+myQuery.map = function(){
+	var ret = [];
+	if( arguments[0] == null || !/\[object Array\]|\[object Object\]/.test( arguments[0] ) ){
+		ret = [arguments[0]];
 	}
+
+	if( Array.isArray(arguments[0]) ){
+		if( arguments.length == 1 || !/\[object Function\]|\[object String\]/.test( arguments[1] )){
+			ret = arguments[0];
+		}else{
+			//todo
+		}
+	}else{
+		ret = [arguments[0]];
+	}
+
+
+
+};
+/*==============================函数式API end ==================================================*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 	if ( typeof noGlobal === strundefined ) {
 		window.myQuery = window.$ = myQuery;
